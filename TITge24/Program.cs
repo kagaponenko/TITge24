ce0dba51cff8383c8563c54ed1e05aa1a51114d2
opilane <opilane@HP-CZC0359NNB>
2024-12-12 14:07:55 +0200

    Add project files.

---
+++ TITge24/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TITge24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ristküliku pindala ja ümbermõõt!");
+            Console.WriteLine("Enter first value");
+
+            string firstNumber = Console.ReadLine();
+            float floatFirst = float.Parse(firstNumber);
+
+            string secondNumber = Console.ReadLine();
+            float secondFirst = float.Parse(secondNumber);
+
+            //teha pindala ja ümbermõõdu kalkulatsioon
+            //float pindala = ??
+            //float umbermoot = ??
+        }
+    }
+}
